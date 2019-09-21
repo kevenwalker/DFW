a90d845862b848123de72d3384fddabe5e854c09
kevenwalker <wk_beyond_myself@163.com>
2019-09-21 23:26:16 +0800

    20190921:修正初始版本最后修改时间

--- base/common.h
+++ base/common.h
@@ -11,7 +11,7 @@
 #define MUDULETOTALNUM 1
 #define MAXLEN 128
 #define VERSION "0.0.0" /* 新增功能版本加1.0.0,功能bug修复版本加0.1.0,代码重构版本加0.0.1 */
-#define VERSIONTIME "2019-08-08" /* 版本最后修改时间 */
+#define VERSIONTIME "2019-09-21" /* 版本最后修改时间 */
 enum {
     DFW_FAILED,
     DFW_SUCCESS,
